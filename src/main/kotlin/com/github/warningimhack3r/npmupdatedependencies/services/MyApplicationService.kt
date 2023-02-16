8a44f80e78c373cbac26904cf33dab554c1d934d
GitHub Action <action@github.com>
2023-02-16 07:55:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/warningimhack3r/npmupdatedependencies/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/warningimhack3r/npmupdatedependencies/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.warningimhack3r.npmupdatedependencies.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.warningimhack3r.npmupdatedependencies.MyBundle
 
 class MyApplicationService {
 
