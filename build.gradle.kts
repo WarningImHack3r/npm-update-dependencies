9401829acf59d22129a17430f4cd856fcc974655
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-10-06 18:12:37 +0200

    Upgrade to template 1.11.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,11 @@ repositories {
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
-    jvmToolchain(11)
+    @Suppress("UnstableApiUsage")
+    jvmToolchain {
+        languageVersion = JavaLanguageVersion.of(11)
+        vendor = JvmVendorSpec.JETBRAINS
+    }
 }
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
