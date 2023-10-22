df218c602030d75e8a6d496be006727ce7ac5db0
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-10-22 20:06:05 +0200

    Attempt to fix logo for NewUI

--- src/main/kotlin/icons/NUDIcons.kt
+++ src/main/kotlin/icons/NUDIcons.kt
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader
 @Suppress("unused")
 object NUDIcons {
     @JvmField
-    val LOGO = IconLoader.getIcon("/icons/npm_16.svg", javaClass)
+    val Logo = IconLoader.getIcon("/icons/npm_16.svg", javaClass)
 }
