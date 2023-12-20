7838ed7706be576d11e09884c062ce60ecb19612
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-12-20 18:00:51 +0100

    Fix running npm command on Windows

--- src/main/kotlin/com/github/warningimhack3r/npmupdatedependencies/backend/engine/ShellRunner.kt
+++ src/main/kotlin/com/github/warningimhack3r/npmupdatedependencies/backend/engine/ShellRunner.kt
@@ -3,7 +3,15 @@ package com.github.warningimhack3r.npmupdatedependencies.backend.engine
 import com.jetbrains.rd.util.printlnError
 
 object ShellRunner {
+    private val failedCommands = mutableSetOf<String>()
+
+    private fun isWindows() = System.getProperty("os.name").lowercase().contains("win")
+
     fun execute(command: Array<String>): String? {
+        val commandName = command.firstOrNull() ?: return null
+        if (isWindows() && failedCommands.contains(commandName)) {
+            command[0] = "$commandName.cmd"
+        }
         return try {
             val process = ProcessBuilder(*command)
                 .redirectOutput(ProcessBuilder.Redirect.PIPE)
@@ -11,7 +19,11 @@ object ShellRunner {
             process.waitFor()
             process.inputStream.bufferedReader().readText()
         } catch (e: Exception) {
-            printlnError("Error while executing \"$command\": ${e.message}")
+            if (isWindows() && !commandName.endsWith(".cmd")) {
+                failedCommands.add(commandName)
+                return execute(arrayOf("$commandName.cmd") + command.drop(1).toTypedArray())
+            }
+            printlnError("Error while executing \"${command.joinToString(" ")}\": ${e.message}")
             null
         }
     }
